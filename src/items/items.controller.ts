1202acda77dfe082f71c6d6362929fd80a557ed0
Bilal K <lalbikhan2014@gmail.com>
2022-07-03 05:06:01 +0500

    DTO

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get , Post, Put, Delete, Body} from '@nestjs/common';
+import { CreateItemsDto } from './dto/create-items.dto';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+    @Get()
+    findAll(): string{
+        return 'Get all items'
+        }
+
+    @Post()
+    create(@Body() CreateItemsDto: CreateItemsDto): string{
+        // return `Name: ${CreateItemsDto.name} Desc: ${CreateItemsDto.description}
+        return CreateItemsDto.name
+    }
+}
